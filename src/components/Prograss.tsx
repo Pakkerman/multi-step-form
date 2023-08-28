b5f840d48ed2c512652189786b45eec4fa20e2c7
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-08-28 14:42:30 +0800

    Style prograss icons

--- src/components/Prograss.tsx
+++ src/components/Prograss.tsx
@@ -1,10 +1,20 @@
+import { useFormStepContext } from "~/contexts/FormStepContext"
+
 export const MobileProgress = () => {
+  const { step } = useFormStepContext()
+
   return (
     <div className="flex justify-center space-x-4 md:hidden ">
-      {["1", "2", "3", "4"].map((item) => (
+      {["1", "2", "3", "4"].map((item, idx) => (
         <button
           key={item}
-          className="border-magnolia h-8 w-8 rounded-full border-2 bg-primary-light-blue"
+          className={`border-magnolia h-8 w-8 rounded-full border-[1px] font-bold
+          ${
+            step === idx
+              ? "text-magnolia border-primary-light-blue bg-primary-light-blue"
+              : "text-neutral-light-gray"
+          }
+         `}
         >
           {item}
         </button>
@@ -14,18 +24,27 @@ export const MobileProgress = () => {
 }
 
 export const DesktopProgress = () => {
+  const { step } = useFormStepContext()
+
   return (
-    <ul className="mx-8 hidden flex-col space-y-6  p-4 pt-[15%] md:flex ">
+    <ul className="mx-8 hidden h-[50%] flex-col justify-between  p-4 pt-[15%] md:flex ">
       {["YOUR INFO", "SELECT PLAN", "ADD-ONS", "SUMMARY"].map((item, idx) => (
-        <li key={item} className="flex items-center space-x-2">
-          <button className="border-magnolia h-8 w-8 rounded-full border-2 bg-primary-light-blue font-bold">
+        <li key={item} className="flex items-center space-x-3 ">
+          <button
+            className={`border-magnolia h-8 w-8 rounded-full border-[1px] font-bold
+           ${
+             step === idx
+               ? "text-magnolia border-primary-light-blue bg-primary-light-blue"
+               : "text-neutral-light-gray"
+           } `}
+          >
             {idx + 1}
           </button>
           <div className="flex flex-col">
-            <p className="text-sm font-normal text-neutral-cool-gray">
+            <p className="text-xs font-normal text-neutral-cool-gray">
               STEP {idx + 1}
             </p>
-            <p className="text-sm font-bold text-neutral-light-gray ">{item}</p>
+            <p className="text-sm font-bold text-neutral-alabaster">{item}</p>
           </div>
         </li>
       ))}
